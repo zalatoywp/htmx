a450b9061aa1369bc70f015735c49bd635eba5c5
Zalatoywp <zalatoytablet@gmail.com>
2024-02-09 01:25:37 +0100

    Agrega configuración de React a Astro

---
+++ src/components/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+import RepoWalker from "./RepoWalker";
+
+
+
+function MiComponente()
+{
+  let location = useLocation();
+  return <RepoWalker />;
+}
+
+function App()
+{
+  return (
+    <Router>
+      <MiComponente />
+    </Router>
+  );
+}
+
+export default App;
