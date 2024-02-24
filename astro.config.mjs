09e2aa8314adb52daaa7afe4b40be59231942808
Zalatoywp <zalatoytablet@gmail.com>
2024-02-24 10:10:23 +0100

    Actualización de configuración de Vercel y Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwind from "@astrojs/tailwind";
 
 //https://astro.build/config
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true
+  },
   integrations: [react(), tailwind()],
   output: "static",
   adapter: vercel(),
