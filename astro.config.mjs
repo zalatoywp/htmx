11656bfaf8155bd326281e6886c36f8afbc12f27
Zalatoywp <zalatoytablet@gmail.com>
2024-03-28 01:52:50 +0100

    Actualizar configuración de Astro para usar el adaptador de Vercel sin servidor y cambiar la carpeta de salida a "server"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -8,6 +8,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
 
   integrations: [react(), tailwind()],
-  output: "static",
+  output: "server",
   adapter: vercel(),
 });
